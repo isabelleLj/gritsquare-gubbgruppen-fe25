14f00c6585f0fc29a5a2d5053b250c3169e33b21
mohammed-alpha91 <mohammad.alpha@gritacademy.se>
2026-03-18 16:57:36 +0100

    getting started with js

--- script/firebaseconfig.js
+++ script/firebaseconfig.js
@@ -1,1 +1,26 @@
-const baseURL = "https://flaskpost-8eeb9-default-rtdb.europe-west1.firebasedatabase.app";
+const baseUrl = "https://flaskpost-8eeb9-default-rtdb.europe-west1.firebasedatabase.app/users";
+
+export async function getAllUsers() {
+  const url = baseUrl + ".json";
+
+  try {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch users: ${res.status} ${res.statusText}`);
+    }
+
+    const userObj = await res.json();
+    return userObj;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return null;
+  }
+}
+
+async function logUsers() {
+  const data = await getAllUsers();
+  console.log(data);
+}
+
+logUsers();
